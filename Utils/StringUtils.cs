c71c665dec240a2c09772b7a34bcd6823c678e74
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2020-12-01 16:36:45 +0100

    Initial commit

---
+++ Utils/StringUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Utils.String
+{
+    /// <summary>
+    /// String-related utilities that the standard string class doesn't provide.
+    /// </summary>
+    public static class StringUtils
+    {
+        /// <summary>
+        /// Returns a copy of this string that's reversed.
+        /// </summary>
+        /// <returns>The string in reverse.</returns>
+        public static string Reverse(this string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        /// <summary>
+        /// Returns a copy of this string that's in alphabetical order.
+        /// </summary>
+        /// <returns>The string in alphabetical order.</returns>
+        public static string Alphabetical(this string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Sort(charArray);
+            return new string(charArray);
+        }
+    }
+}
